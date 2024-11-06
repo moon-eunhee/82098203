eab4df43f514dbe2585cc8a2c856328179ffd429
moon-eunhee <ehmoon25@gmail.com>
2024-11-07 01:11:38 +0900

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,3 +1,5 @@
+package com.example.demo.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
